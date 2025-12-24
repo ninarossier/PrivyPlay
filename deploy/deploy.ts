aa315259b29e8f8379724932f76d83999c98c47d
ninarossier <wbgzrua307@outlook.com>
2025-12-24 13:14:21 +0800

    Revise deployment script for the game

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedPrivyPlay = await deploy("PrivyPlay", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`PrivyPlay contract: `, deployedPrivyPlay.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_privyplay"; // id required to prevent reexecution
+func.tags = ["PrivyPlay"];
